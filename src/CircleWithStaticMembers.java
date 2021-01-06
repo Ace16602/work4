41a966dd862fe001c0a5720ddab8963275c897c4
Egg <1209346894@qq.com>
2021-01-06 20:42:28 +0800

    work4

---
+++ src/CircleWithStaticMembers.java
@@ -0,0 +1,15 @@
+public class CircleWithStaticMembers {
+    double radius;
+    static int numberOfObjects=0;
+    CircleWithStaticMembers(){
+        radius=1;
+        numberOfObjects++;
+    }
+    CircleWithStaticMembers(double newRadius){
+        radius=newRadius;
+        numberOfObjects++;
+    }
+    double getArea(){
+        return radius*radius*Math.PI;
+    }
+}
---
+++ src/TestPassObject.java
@@ -0,0 +1,16 @@
+public class TestPassObject {
+    public static void main(String[] args) {
+        CircleWithPrivateDataFields myCircle = new CircleWithPrivateDataFields(1);
+        int n = 5;
+        System.out.println("\n"+"Radius is "+myCircle.getRadius());
+        System.out.println("n is "+n);
+    }
+
+    public static void printAreas(CircleWithPrivateDataFields c, int times) {
+        System.out.println("Radius \t\tArea");
+        while (times >= 1) {
+            System.out.println(c.getRadius() + "\t\t" + c.getArea());
+            c.setRadius(c.getRadius()+1);
+        }
+    }
+}
